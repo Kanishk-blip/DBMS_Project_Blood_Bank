d51e1eaa3ee4e3489af654a9365819df508585f7
Kanishk Agarwal <agarwalkanishk9@gmail.com>
2025-05-07 01:09:02 +0530

    add

--- Backend/controllers/getCitiesWithDonorsAndRecipients.js
+++ Backend/controllers/getCitiesWithDonorsAndRecipients.js
@@ -19,7 +19,7 @@ const getCitiesWithDonorsAndRecipients = async (req, res) => {
       console.log("Rows:", result.rows);
   
       await connection.close();
-      res.json(result.rows);
+      res.json(result.rows); 
     } catch (err) {
       console.error('Error executing query:', err);
       res.status(500).json({ error: 'Internal server error' });
